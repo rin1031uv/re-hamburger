62b84968dc1d42dd16290707e3591e131278d1a7
Rina <apsws.paws@gmail.com>
2023-08-03 18:03:25 +0900

    ドロワーメニューPC時チェックボックスの機能を切る実装追加

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
-
+$(document).ready(function(){
+  if(window.matchMedia('(min-width: 1201px)').matches) {
+    document.getElementsById("drawer-checkbox").checked = false;
+  }
+});
 
 //動作確認用
 $(function(){
